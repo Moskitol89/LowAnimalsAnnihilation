dd8aa84550c5529463a5cc00f9c0c18c79fa4be7
Vladimir Kurganov <Kurganovv@yahoo.com>
2022-10-23 18:03:59 +0300

    init

---
+++ src/LowAnimalsAnnihilation.java
@@ -0,0 +1,14 @@
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+
+@ScriptManifest(name = "Low animals annihilation", description = "Chicken Cows e.t.c. Annihilation", author = "Moskitol89",
+        version = 1.0, category = Category.COMBAT, image = "")
+public class LowAnimalsAnnihilation extends AbstractScript {
+
+
+    @Override
+    public int onLoop() {
+        return 0;
+    }
+}
